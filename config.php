08b964d845b0262371dd5f3ed0267ece3ddcc5b1
Blount <blount@ilatumi.org>
2013-02-24 22:01:28 +0100

    ajout de la configuration du proxy et de l'user agent à utiliser.

--- config.php
+++ config.php
@@ -9,6 +9,18 @@
 define("MULTI_USER", false); // valeur true ou false
 
 
+define("USER_AGENT", "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.6) Gecko/20100628 Ubuntu/10.04 (lucid) Firefox/3.6.6");
+
+
+/**
+ * PROXY
+ */
+// adresse du proxy
+define("PROXY_IP", "");
+// port du proxy
+define("PROXY_PORT", 0);
+
+
 // on affiche les erreurs PHP
 ini_set("display_errors", true);
 
